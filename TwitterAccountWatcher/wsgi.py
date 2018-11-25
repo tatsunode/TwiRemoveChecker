b20d6ca144077ac7c975f79dfdbfa4c514bb9f65
tatsunode <tatsunode@yahoo.co.jp>
2018-11-26 02:07:24 +0900

    v2-deployed

--- TwitterAccountWatcher/wsgi.py
+++ TwitterAccountWatcher/wsgi.py
@@ -6,8 +6,15 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
+#/usr/bin/python3
 
 import os
+import sys
+import site
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+site.addsitedir(os.path.join(PROJECT_ROOT, 'ENV', 'lib', 'python3.5', 'site-packages'))
+sys.path.insert(0, PROJECT_ROOT)
 
 from django.core.wsgi import get_wsgi_application
 
